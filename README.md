95f886ad7c2c990a7116886747c0349f68884cb1
Alex Rose <alex@falconcctv.com>
2021-02-22 04:02:51 +0900

    finishing touches

--- README.md
+++ README.md
@@ -1,10 +1,13 @@
 # baby_names
 
 ## No Arguments
-### If you just run the script, baby_names.py will search the data/ directory for files to parse and return a list
+### Run the script with no arguments, baby_names.py will search the data/ directory for files to parse and return a list
 
 ## Specify a file
-### Include "data/{filename}" as a argument and it will parse the file and return a dictionary of that year separated by boys names, girls names, and their popularity in alphabetical order.
+### Include "data/{filename}" as a argument to parse the file and return a dictionary separated by boys names, girls names, and their popularity in alphabetical order.
 
-## --all (beta)
-### If you pass '--all' as an argument, baby_names.py will search the entire data/ directory and include all files in the return dictionary.
+## "--all"
+### Pass '--all' as an argument, baby_names.py will search the entire /data directory and include all files
+
+## launch.json
+### See the /.vscode/launch.json file for examples of how to run this in VS Code debugger
--- baby_names.py
+++ baby_names.py
@@ -2,7 +2,7 @@ import re, sys, os
 
 
 def baby_names_parser(filename):
-    print(filename)
+    print(f'File: {filename}')
     try: 
         with open(filename, 'r',encoding = 'utf-8') as f:
             ingest = f.read()
@@ -10,28 +10,19 @@ def baby_names_parser(filename):
     except:
         print(f'Could not find {filename}')
         return False
-
-    # parsed_file = ingest
-    # return parsed_file
-
-    year = re.search(r'Popularity\sin\s(\d{4})', ingest).group()[-4:]    
-
-    # <tr align="right"><td>1</td><td>Michael</td><td>Jessica</td>
-    raw_list = re.findall(r'<td>(\d+)</td><td>(\w+)</td><td>(\w+)</td>', ingest)
     
     boy = {}
     girl = {}
 
-    for line in raw_list:
-        boy[line[1]] = int(line[0])
-        girl[line[2]] = int(line[0])
+    for rank, boy_name, girl_name in re.findall(r'<td>(\d+)</td><td>(\w+)</td><td>(\w+)</td>', ingest): # create tuples of the table rows (rank, boy_name, girl_name)
+        boy[boy_name] = int(rank) # make the boy dict
+        girl[girl_name] = int(rank) # make the girl dict
     
-    names[year]= {"Male": boy, "Female": girl}
-    # print(names)
+    names[re.search(r'Popularity\sin\s(\d{4})', ingest).group()[-4:]]= {"Male": boy, "Female": girl} #add a 'year' entry to the existing dict instead of returning something
 
 def main():
     global names
-    names = {
+    names = { # before writing any other code, a data structure was decided
     # '2004':{
     #     'boy':{
     #         "Alex": 4,
@@ -62,27 +53,40 @@ def main():
     #     }
     }
 
-    print('Baby Name Parser')
+    print('Baby Name Popularity Parser')
     args = sys.argv[1:]
-    if not args:
+    
+    if not args: # No arguments returns a list of files available for parsing then exits
         print(f'Current working directory is: {os.getcwd()}')
-        print(f'Files available for parsing are:')
+        print(f'Specify one or more of the following files:')
         [print(f'data/{name}') for name in os.listdir(path='data/')]
-        print('Or "--all" to recurse the entire data/ directory')
-    elif args == '--all':
-        args = os.listdir(path='data/')
-        os.chdir('data/')
-    else:
-        for filename in args:
-            baby_names_parser(filename)
+        print('Or "--all" to recurse the data/ directory')
+        exit()
+    
+    if args[0] == '--all': # "--all" walks the /data directory and returns all files
+        print("You want the whole enchalada")
+        files = os.listdir(path='data/')
+        args = [] # need it clean for the new list of files
+        for item in files:
+            args.append('data/' + item)
+        
+    for filename in args: # can pass more than one filename
+        baby_names_parser(filename)
 
-        if names:
-            for year in names.keys():
-                print(f'\nYear: {year}')
-                for gender in names[year]:
-                    print(f'\n\tGender: {gender}')
-                    for name in sorted(names[year][gender], key=str.lower)[:9]:
-                        print(f'\t\t{name}: {names[year][gender][name]}')
+    if names: # Output the names by Year / Gender in alphabetical order
+        for year in names.keys():
+            print(f'\nYear: {year}')
+            for gender in names[year]:
+                print(f'\n\tGender: {gender}')
+                for name in sorted(names[year][gender], key=str.lower)[:50]: # only displaying the first 10
+                    print(f'\t\t{name}: {names[year][gender][name]}')
+
+    print('\n\nNoah-') #check the popularity of a specific name over time
+    for year in names.keys():
+        try:
+            print(f'Year: {year} Rank: {names[year]["Male"]["Noah"]}')
+        except:
+            print("Name not found")
 
 if __name__ == '__main__':
     main()

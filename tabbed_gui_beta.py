7d42c98b1839bb81f28c1484b60d09fbc2c8d7e3
Alex <alex@falconcctv.com>
2021-03-01 19:30:34 +0900

    time for class

--- tabbed_gui_beta.py
+++ tabbed_gui_beta.py
@@ -5,9 +5,6 @@ import os
 import baby_names
 
 
-from PIL import ImageTk, Image
-
-
 
 def file_chooser():
     global window, files_chosen, names
@@ -20,20 +17,35 @@ def file_chooser():
     for file in window.filename:
         names.update(baby_names.baby_names_parser(file))
 
-    years_list = names.keys()
+    # year_selection = IntVar()
 
-    years = [(str(year),str(year)) for year in years_list]
-
-    year_selection = IntVar()
-
-    [Radiobutton(file_parser_frame_left, text=year, variable=year_selection, value=years).grid(sticky=N) for year,year, in years]
+    # [Radiobutton(file_parser_frame_left, text=year, variable=year_selection, value=years).grid(sticky=N) for year,year, in years]
 
     scrollable_text = scrolledtext.ScrolledText(file_parser_frame_right, width=40, height=45)
     scrollable_text.grid(column=7, columnspan=5, row=0, sticky=E)
 
     scrollable_text.insert(INSERT, [f'Name: {name[0]} \t Rank: {name[1]}\n' for name in baby_names.output_names(names, order='rank')])
     scrollable_text.configure(state="disabled")
+    populate_year_checks()
     
+def update_text(year_choice, order_selection, gender_selection):
+    global scrollable_text
+    scrollable_text = scrolledtext.ScrolledText(file_parser_frame_right, width=40, height=45)
+    scrollable_text.grid(column=7, columnspan=5, row=0, sticky=E)
+
+    scrollable_text.insert(INSERT, [f'Name: {name[0]} \t Rank: {name[1]}\n' for name in baby_names.output_names(names, years=year_choice, order=order_selection, gender=gender_selection)])
+    scrollable_text.configure(state="disabled")
+
+
+def populate_year_checks():
+    check_box_frame = LabelFrame(file_parser_frame_left, padx=5, pady=5)
+    check_box_frame.grid(row=3, column=0, sticky=S)
+
+    years_var_list = [StringVar(value=1) for year in names.keys()]
+    years_check_boxes = [Checkbutton(check_box_frame, text=year, variable=year_var, command=update_text) for year, year_var in zip(names.keys(),years_var_list)]
+
+    for check_box in years_check_boxes:
+        check_box.pack()
 
 def main():
     global window, my_notebook, my_frame, file_parser, clipboard_parser, regex_parser, names, file_parser_frame_left, file_parser_frame_right
@@ -60,17 +72,18 @@ def main():
     my_notebook.add(clipboard_parser, text="Clipboard Parser")
     my_notebook.add(regex_parser, text="Regex Explorer")
 
-    file_parser_frame_left = LabelFrame(file_parser, padx=5, pady=5)
-    file_parser_frame_left.grid(row=0, column=0, padx=5, pady=5)
+    file_parser_frame_left = LabelFrame(file_parser, height=700, padx=5, pady=5)
+    file_parser_frame_left.grid(row=0, column=0, sticky=N+W, )
     file_parser_frame_right = LabelFrame(file_parser, padx=5, pady=5)
-    file_parser_frame_right.pack(row=0, column=1, padx=5, pady=5)
+    file_parser_frame_right.grid( row=0, column=5, stick=N+E)
 
 
-    open_files = Button(file_parser_frame_left, text="Select Files", command=file_chooser, padx=10, pady=5).grid(row=0, column=0, padx=10, pady=10, sticky=N+W)
+    open_files = Button(file_parser_frame_left, text="Select Files", command=file_chooser, padx=10, pady=5)
+    open_files.grid(padx=10, pady=10, sticky=N+W, row=0, column=0)
 
 
 
-    # years = 
+
 
 
     window.mainloop()
